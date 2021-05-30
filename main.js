ac5320eb8064ec92c1482b0a6096c02fc58fd64a
sam-tj <sameer.tuteja05@gmail.com>
2021-05-30 19:25:03 +0200

    js fix

--- main.js
+++ main.js
@@ -5,35 +5,58 @@ function sendMovementData(direction) {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("currentDirection").innerHTML =
         "Current State " + this.responseText;
+      if (this.responseText == "stop") {
+        document.getElementById("powerSwitch").checked = false;
+        document.getElementById("slider-id-01").value = 0;
+        document.getElementById("fwd_button").setAttribute("disabled", "");
+        document.getElementById("lft_button").setAttribute("disabled", "");
+        document.getElementById("rgt_button").setAttribute("disabled", "");
+        document.getElementById("bwd_button").setAttribute("disabled", "");
+      }
     }
   };
   xhttp.open("GET", "setDIR?Motionstate=" + direction, true);
   xhttp.send();
 }
 function sendPowerData() {
-  console.log(
-    "sendPowerData " + document.getElementById("powerSwitch").checked
-  );
+  //console.log(
+  //"sendPowerData " + document.getElementById("powerSwitch").checked
+  //);
+  console.log(document.getElementById("slider-id-01").value);
   var state = document.getElementById("powerSwitch").checked;
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      if (this.responseText == 1)
+      console.log(
+        "sendPowerData " + document.getElementById("powerSwitch").checked
+      );
+      if (this.responseText == "true") {
         document.getElementById("powerSwitch").checked = true;
-      else if (this.responseText == 0)
+        document.getElementById("fwd_button").removeAttribute("disabled");
+        document.getElementById("lft_button").removeAttribute("disabled");
+        document.getElementById("rgt_button").removeAttribute("disabled");
+        document.getElementById("bwd_button").removeAttribute("disabled");
+      } else if (this.responseText == "false") {
         document.getElementById("powerSwitch").checked = false;
+        document.getElementById("slider-id-01").value = 0;
+        document.getElementById("fwd_button").setAttribute("disabled", "");
+        document.getElementById("lft_button").setAttribute("disabled", "");
+        document.getElementById("rgt_button").setAttribute("disabled", "");
+        document.getElementById("bwd_button").setAttribute("disabled", "");
+      }
     }
   };
   xhttp.open("GET", "setPower?Powerstate=" + state, true);
   xhttp.send();
 }
 function sendSpeedData(speed) {
-  console.log("sendSpeedData " + speed);
+  //console.log("sendSpeedData " + speed);
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("currentDirection").innerHTML =
-        "Going " + this.responseText;
+      // document.getElementById("currentDirection").innerHTML =
+      //   "Going " + this.responseText;
+      console.log("sendSpeedData " + speed);
     }
   };
   xhttp.open("GET", "setSpeed?Speedstate=" + speed, true);
