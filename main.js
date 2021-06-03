692f59b39da8902035ae2c08c012e5a26aea467d
sam-tj <sameer.tuteja05@gmail.com>
2021-06-03 13:50:59 +0200

    minor fix in logic

--- main.js
+++ main.js
@@ -1,18 +1,72 @@
 var lastDirection = "...";
-var lastSpeed = 0;
+var timeVal = "";
+
+var en_us = {
+  welcome: "Welcome",
+  title: "Mover Dashboard",
+  battery: "Battery Percentage 🔋",
+  workTime: "Work Time",
+  currentTime: "Current Time",
+  powerLabel: "Power",
+  powerStateOff: "Off",
+  powerStateOn: "On",
+  emergencyStop: "Emergency Stop",
+  settingsModal: "Settings",
+  updateSystemButton: "Update System",
+  languageButton: "Languages",
+  moreComing: "More coming soon...",
+  doneSettingButton: "Done",
+  doneLanguageButton: "Done",
+  languageModal: "Language",
+  currenttStateOfBot_moving: "Current State: Moving",
+  currenttStateOfBot_not_moving: "Current State: Not Moving",
+  currenttStateOfBot_emergency: "Current State: Emergency Stop",
+  selectOption00: "Select...",
+  selectOption01: "Morning by 8 am",
+  selectOption02: "Afternoon by 12 pm",
+  selectOption03: "Evening by 4 pm",
+  selectOption04: "Night by 8 pm",
+};
+var de_de = {
+  welcome: "Wilkommen",
+  title: "Mover Dashboard",
+  battery: "Batterieprozent 🔋",
+  workTime: "Arbeit Zeit",
+  currentTime: "Strom Zeit",
+  powerLabel: "Strom",
+  powerStateOff: "aus",
+  powerStateOn: "an",
+  emergencyStop: "Notaus-Knopf",
+  settingsModal: "Einstellungen",
+  updateSystemButton: "System aktualisieren",
+  languageButton: "Sprachen",
+  moreComing: "Mehr kommt bald...",
+  doneSettingButton: "Fertig",
+  doneLanguageButton: "Fertig",
+  languageModal: "Sprache",
+  currenttStateOfBot_moving: "Aktuellen Zustand: Moving",
+  currenttStateOfBot_not_moving: "Aktuellen Zustand: Not Moving",
+  currenttStateOfBot_emergency: "Aktuellen Zustand: Stop",
+  selectOption00: "wählen...",
+  selectOption01: "Morgens um 8 Uhr",
+  selectOption02: "Nachmittag um 12 Uhr",
+  selectOption03: "Abends um 16 Uhr",
+  selectOption04: "Nacht um 20 Uhr",
+};
+
+var langChange = en_us;
+
 function sendMovementData(direction) {
-  console.log("sendMovementData " + direction);
   lastDirection = direction;
   if (direction == "stop") {
     document.getElementById("powerSwitch").checked = false;
     document.getElementById("currentDirection").innerHTML =
-      "Current State: Emergency Stop";
+      langChange.currenttStateOfBot_emergency;
+    document.getElementById("emergencyStopTitle").setAttribute("disabled", "");
   }
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("currentDirection").innerHTML =
-        "Current State " + this.responseText;
       if (this.responseText == "stop") {
         document.getElementById("powerSwitch").checked = false;
       }
@@ -21,35 +75,35 @@ function sendMovementData(direction) {
   xhttp.open("GET", "setDIR?Motionstate=" + direction, true);
   xhttp.send();
 }
-function sendPowerData() {
-  //console.log(
-  //"sendPowerData " + document.getElementById("powerSwitch").checked
-  //);
 
-  var state = document.getElementById("powerSwitch").checked;
-  if (state == true) {
-    document.getElementById("currentDirection").innerHTML =
-      "Current State: Moving";
-  } else if (state == false) {
-    document.getElementById("currentDirection").innerHTML =
-      "Current State: Not Moving";
+function onSelectorChange(value) {
+  console.log(value);
+  if (value > 0) {
+    document.getElementById("powerSwitch").removeAttribute("disabled");
+  } else {
+    document.getElementById("powerSwitch").checked = false;
+    document.getElementById("powerSwitch").setAttribute("disabled", "");
+    document.getElementById("emergencyStopTitle").setAttribute("disabled", "");
   }
+}
+
+function sendPowerData() {
+  var state = document.getElementById("powerSwitch").checked;
   console.log(state);
   if (state == true) {
     document.getElementById("powerSwitch").checked = true;
     document.getElementById("currentDirection").innerHTML =
-      "Current State: Moving";
+      langChange.currenttStateOfBot_moving;
+    document.getElementById("emergencyStopTitle").removeAttribute("disabled");
   } else if (state == false) {
     document.getElementById("powerSwitch").checked = false;
     document.getElementById("currentDirection").innerHTML =
-      "Current State: Not Moving";
+      langChange.currenttStateOfBot_not_moving;
+    document.getElementById("emergencyStopTitle").setAttribute("disabled", "");
   }
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      console.log(
-        "sendPowerData " + document.getElementById("powerSwitch").checked
-      );
       if (this.responseText == "true") {
         document.getElementById("powerSwitch").checked = true;
       } else if (this.responseText == "false") {
@@ -58,22 +112,6 @@ function sendPowerData() {
     }
   };
   xhttp.open("GET", "setPower?Powerstate=" + state, true);
-  xhttp.send();
-}
-function sendSpeedData(speed) {
-  console.log("sendSpeedData " + speed);
-  lastSpeed = speed;
-  document.getElementById("currentDirection").innerHTML =
-    "Current State " + lastDirection + " at speed " + lastSpeed;
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      // document.getElementById("currentDirection").innerHTML =
-      //   "Going " + this.responseText;
-      console.log("sendSpeedData " + speed);
-    }
-  };
-  xhttp.open("GET", "setSpeed?Speedstate=" + speed, true);
   xhttp.send();
 }
 
@@ -119,7 +157,7 @@ function checkTime(i) {
   }
   return i;
 }
-var timeVal = "";
+
 function startTime() {
   var today = new Date();
   var h = today.getHours();
@@ -136,47 +174,11 @@ function startTime() {
 }
 startTime();
 
-var en_us = {
-  welcome: "Welcome",
-  title: "Mover Dashboard",
-  battery: "Battery Percentage 🔋",
-  workTime: "Work Time",
-  currentTime: "Current Time",
-  powerLabel: "Power",
-  powerStateOff: "Off",
-  powerStateOn: "On",
-  emergencyStop: "Emergency Stop",
-  settingsModal: "Settings",
-  updateSystemButton: "Update System",
-  languageButton: "Languages",
-  moreComing: "More coming soon...",
-  doneSettingButton: "Done",
-  doneLanguageButton: "Done",
-  languageModal: "Language",
-};
-var de_de = {
-  welcome: "Wilkommen",
-  title: "Mover Dashboard",
-  battery: "Batterieprozent 🔋",
-  workTime: "Arbeit Zeit",
-  currentTime: "Strom Zeit",
-  powerLabel: "Strom",
-  powerStateOff: "aus",
-  powerStateOn: "an",
-  emergencyStop: "Stop",
-  settingsModal: "Einstellungen",
-  updateSystemButton: "System aktualisieren",
-  languageButton: "Sprachen",
-  moreComing: "Mehr kommt bald...",
-  doneSettingButton: "Fertig",
-  doneLanguageButton: "Fertig",
-  languageModal: "Sprache",
-};
 function changeLanguage(lang) {
   if (lang == "en") {
-    var langChange = en_us;
+    langChange = en_us;
   } else if (lang == "de") {
-    var langChange = de_de;
+    langChange = de_de;
   }
   //console.log(lang);
   document.getElementById("mainTitle").innerHTML = langChange.title;
@@ -205,6 +207,16 @@ function changeLanguage(lang) {
     langChange.languageModal;
   document.getElementById("doneLanguageTitle").innerHTML =
     langChange.doneLanguageButton;
-  var modalLanguage = document.getElementById("modalLanguage");
-  modalLanguage.style.display = "none";
+  document.getElementById("select-01-op-00").innerHTML =
+    langChange.selectOption00;
+  document.getElementById("select-01-op-01").innerHTML =
+    langChange.selectOption01;
+  document.getElementById("select-01-op-02").innerHTML =
+    langChange.selectOption02;
+  document.getElementById("select-01-op-03").innerHTML =
+    langChange.selectOption03;
+  document.getElementById("select-01-op-04").innerHTML =
+    langChange.selectOption04;
+
+  document.getElementById("modalLanguage").style.display = "none";
 }
