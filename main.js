22986a6895c278d2682baa29cd0236a4f82ddc3f
sam-tj <sameer.tuteja05@gmail.com>
2021-06-23 02:08:41 +0200

    notification FIx

--- main.js
+++ main.js
@@ -231,6 +231,22 @@ function notificationButton() {
     askForApproval();
   }
 }
+navigator.serviceWorker.register("sw.js");
+
+// function showNotification() {
+//   Notification.requestPermission(function (result) {
+//     if (result === "granted") {
+//       navigator.serviceWorker.ready.then(function (registration) {
+//         registration.showNotification("Vibration Sample", {
+//           body: "Buzz! Buzz!",
+//           icon: "../images/touch/chrome-touch-icon-192x192.png",
+//           vibrate: [200, 100, 200, 100, 200, 100, 200],
+//           tag: "vibration-sample",
+//         });
+//       });
+//     }
+//   });
+// }
 function askForApproval() {
   Notification.requestPermission((permission) => {
     if (permission === "granted") {
@@ -243,8 +259,14 @@ function askForApproval() {
   });
 }
 function createNotification(title, text) {
-  const noti = new Notification(title, {
-    body: text,
+  // const noti = new Notification(title, {
+  //   body: text,
+  // });
+  navigator.serviceWorker.ready.then(function (registration) {
+    registration.showNotification(title, {
+      body: text,
+      vibrate: [200, 100, 200, 100, 200, 100, 200],
+    });
   });
 }
 function notificationsCat(value) {
